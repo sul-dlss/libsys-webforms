4d95223b6a319d49ca479c2077d2f2dbc890bf72
Joshua Greben <jgreben@stanford.edu>
2016-03-08 12:07:45 -0800

    Proper behavior for current login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
+    @current_user ||= begin
+      return unless user_id.present?
+      user_id
+    end
+  end
+  helper_method :current_user
+
   def user_id
     request.env['REMOTE_USER'] || ENV['REMOTE_USER']
   end
