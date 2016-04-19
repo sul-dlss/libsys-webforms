39bb4f598e2e1f8891a59c9e68245e4f7c789b78
Anthony Zanella <azanella@stanford.edu>
2016-04-19 16:48:26 -0700

    Instead of REMOTE_USER, login and logout authorized_user

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 describe ApplicationController do
   before do
-    request.env['REMOTE_USER'] = 'some-user'
-    allow(controller.request).to receive(:env).and_return('REMOTE_USER' => 'some-user')
+    request.env['REMOTE_USER'] = 'testuser'
+    allow(controller.request).to receive(:env).and_return('REMOTE_USER' => 'testuser')
   end
   describe '#user_id' do
     it 'should return a user_id when there is a user in the environment' do
       user = controller.send(:user_id)
-      expect(user).to eq('some-user')
+      expect(user).to eq('testuser')
     end
   end
   describe '#current_user' do
     it 'should set the current_user as a webauth_user' do
       user = controller.send(:user_id)
-      current_user = controller.send(:current_user)
-      expect(current_user).to eq(user)
+      current_user = FactoryGirl.create(:authorized_user)
+      expect(current_user.user_id).to eq(user)
     end
   end
 end
