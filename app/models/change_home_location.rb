937369ce7713c5512f4b53eac3d42cc8a333c989
Anthony Zanella <azanella@stanford.edu>
2016-06-14 11:47:36 -0700

    change_home_location creates a batch

--- app/models/change_home_location.rb
+++ app/models/change_home_location.rb
@@ -11,5 +11,11 @@ class ChangeHomeLocation
   attr_accessor :item_ids
   attr_accessor :email
   attr_accessor :comments
+  attr_accessor :batch_date
+  attr_accessor :user_name
+  attr_accessor :action
+  attr_accessor :priority
+  attr_accessor :export_yn
+  attr_accessor :check_bc_first
   validates :current_library, :new_home_location, :item_ids, presence: true
 end
