35c6cee831da15bfb7102737e2009a7e960f79a7
Anthony Zanella <azanella@stanford.edu>
2016-06-16 14:17:19 -0700

    Roll mailer out to other controllers

--- app/controllers/transfer_items_controller.rb
+++ app/controllers/transfer_items_controller.rb
@@ -5,18 +5,21 @@ class TransferItemsController < ApplicationController
     @transfer_item = TransferItem.new
   end
 
+  # rubocop:disable Metrics/MethodLength
   def create
     @transfer_item = TransferItem.new(params[:transfer_item])
     if @transfer_item.valid?
       array_of_item_ids = @transfer_item.parse_uploaded_file
       @uni_updates_batch = UniUpdatesBatch.create_transfer_item_batch(params)
       UniUpdates.create_for_batch(array_of_item_ids, @uni_updates_batch)
+      WebformsMailer.batch_upload_email(@uni_updates_batch).deliver_now
       redirect_valid
     else
       render action: 'new'
       flash[:error] = 'Check that all fields are entered correctly'
     end
   end
+  # rubocop:enable Metrics/MethodLength
 
   def redirect_valid
     flash[:notice] = 'Batch uploaded!'
--- app/controllers/withdraw_items_controller.rb
+++ app/controllers/withdraw_items_controller.rb
@@ -5,16 +5,19 @@ class WithdrawItemsController < ApplicationController
     @withdraw_item = WithdrawItem.new
   end
 
+  # rubocop:disable Metrics/MethodLength
   def create
     @withdraw_item = WithdrawItem.new(params[:withdraw_item])
     if @withdraw_item.valid?
       array_of_item_ids = @withdraw_item.parse_uploaded_file
       @uni_updates_batch = UniUpdatesBatch.create_withdraw_item_batch(params)
       UniUpdates.create_for_batch(array_of_item_ids, @uni_updates_batch)
+      WebformsMailer.batch_upload_email(@uni_updates_batch).deliver_now
       flash[:notice] = 'Batch uploaded!'
       redirect_to @uni_updates_batch
     else
       render action: 'new'
     end
   end
+  # rubocop:enable Metrics/MethodLength
 end
