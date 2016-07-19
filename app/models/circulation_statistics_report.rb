bcffbf6234cc87a8f1a784eb4d3dcc2ea6c8c3c3
Anthony Zanella <azanella@stanford.edu>
2016-07-18 17:36:50 -0700

    Create js menu for circ_stats_report

--- app/models/circulation_statistics_report.rb
+++ app/models/circulation_statistics_report.rb
@@ -1,5 +1,7 @@
 # app/models/circulation_statistics_report.rb
 class CirculationStatisticsReport
   include ActiveModel::Model
-  attr_accessor :email, :libraries, :source, :range_type, :low_callnum_range, :hi_callnum_range
+  attr_accessor :email, :lib_array, :source, :range_type, :call_lo, :call_hi,
+                :call_alpha, :barcodes
+  validates :email, :lib_array, :call_lo, presence: true
 end
