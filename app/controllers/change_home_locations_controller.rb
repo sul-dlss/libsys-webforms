e6ed02f472ebf3e3e153a08d1d73ffc491184236
Anthony Zanella <azanella@stanford.edu>
2016-08-29 11:44:34 -0700

    Handle item id dups. This closes #152, closes #115, and closes #84

--- app/controllers/change_home_locations_controller.rb
+++ app/controllers/change_home_locations_controller.rb
@@ -5,19 +5,22 @@ class ChangeHomeLocationsController < ApplicationController
     @change_home_location = ChangeHomeLocation.new
   end
 
-  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
   def create
     @change_home_location = ChangeHomeLocation.new(params[:change_home_location])
     if @change_home_location.valid?
       array_of_item_ids = @change_home_location.parse_uploaded_file
-      @uni_updates_batch = UniUpdatesBatch.create_home_location_batch(params, array_of_item_ids.count)
-      UniUpdates.create_for_batch(array_of_item_ids, @uni_updates_batch)
-      WebformsMailer.batch_upload_email(@uni_updates_batch).deliver_now
+      filtered_item_ids = UniUpdates.filter_duplicates(array_of_item_ids)
+      uniques = filtered_item_ids[0]
+      duplicates = filtered_item_ids[1].join(' ')
+      @uni_updates_batch = UniUpdatesBatch.create_home_location_batch(params, uniques.count)
+      UniUpdates.create_for_batch(uniques, @uni_updates_batch)
+      WebformsMailer.batch_upload_email(@uni_updates_batch, duplicates).deliver_now
       flash[:notice] = 'Batch uploaded!'
-      redirect_to @uni_updates_batch
+      redirect_to uni_updates_batch_path(@uni_updates_batch, duplicates: duplicates)
     else
       render action: 'new'
     end
   end
-  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/MethodLength,Metrics/AbcSize
 end
--- app/controllers/change_item_types_controller.rb
+++ app/controllers/change_item_types_controller.rb
@@ -5,19 +5,22 @@ class ChangeItemTypesController < ApplicationController
     @change_item_type = ChangeItemType.new
   end
 
-  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
   def create
     @change_item_type = ChangeItemType.new(params[:change_item_type])
     if @change_item_type.valid?
       array_of_item_ids = @change_item_type.parse_uploaded_file
-      @uni_updates_batch = UniUpdatesBatch.create_item_type_batch(params, array_of_item_ids.count)
-      UniUpdates.create_for_batch(array_of_item_ids, @uni_updates_batch)
-      WebformsMailer.batch_upload_email(@uni_updates_batch).deliver_now
+      filtered_item_ids = UniUpdates.filter_duplicates(array_of_item_ids)
+      uniques = filtered_item_ids[0]
+      duplicates = filtered_item_ids[1].join(' ')
+      @uni_updates_batch = UniUpdatesBatch.create_item_type_batch(params, uniques.count)
+      UniUpdates.create_for_batch(uniques, @uni_updates_batch)
+      WebformsMailer.batch_upload_email(@uni_updates_batch, duplicates).deliver_now
       flash[:notice] = 'Batch uploaded!'
-      redirect_to @uni_updates_batch
+      redirect_to uni_updates_batch_path(@uni_updates_batch, duplicates: duplicates)
     else
       render action: 'new'
     end
   end
-  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/MethodLength,Metrics/AbcSize
 end
