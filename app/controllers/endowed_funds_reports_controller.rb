06c200c6e0a0e8d8eefac3b5bed3233e18d016e3
Joshua Greben <jgreben@stanford.edu>
2016-09-08 09:23:36 -0700

    Add MVC for enbdowed funds management report

---
+++ app/controllers/endowed_funds_reports_controller.rb
@@ -0,0 +1,25 @@
+###
+# Controller to handle the Encumberances Report
+###
+class EndowedFundsReportsController < ApplicationController
+  def new
+    @endowed_funds_report = EndowedFundsReport.new
+  end
+
+  # 1_ count the number of funds and flash error if more than 50
+  # 2_
+  def create
+    @endowed_funds_report = EndowedFundsReport.new(batch_params)
+    if @endowed_funds_report.valid?
+      flash[:success] = 'Report requested!'
+      # foooo = EndowedFundsReport.foo
+    else
+      flash[:warning] = 'Check that all form fields are entered!'
+    end
+    redirect_to root_path
+  end
+
+  def batch_params
+    params.require(:endowed_funds_report).permit!
+  end
+end
