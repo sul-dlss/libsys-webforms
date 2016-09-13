5948a1d6ca4bc66b570a49fab55af5d28f71cdd3
Anthony Zanella <azanella@stanford.edu>
2016-09-12 17:25:54 -0700

    Add additional validations

--- app/models/shelf_selection_report.rb
+++ app/models/shelf_selection_report.rb
@@ -14,6 +14,10 @@ class ShelfSelectionReport
   validates :loc_array, length: { minimum: 2, message: "can't be empty" }
   validates :call_lo, presence: true, if: :lc_range_type?
   validates :fmt_array, :itype_array, length: { minimum: 2, message: "can't be empty" }, if: :lc_range_type?
+  validate :classic_call_lo_and_hi, if: :classic_range_type?
+  validates :itype_array, length: { minimum: 2, message: "can't be empty" }, if: :classic_range_type?
+  validates :fmt_array, length: { minimum: 2, message: "can't be empty" }, if: :other_range_type?
+  validates :fmt_array, length: { minimum: 2, message: "can't be empty" }, if: :other_range_type?
 
   def self.generic_options
     [["Doesn't matter", 0], ['INCLUDE only', 1], ['EXCLUDE', 2]]
@@ -23,5 +27,20 @@ class ShelfSelectionReport
 
   def lc_range_type?
     range_type == 'lc'
+  end
+
+  def classic_range_type?
+    range_type == 'classic'
+  end
+
+  def other_range_type?
+    range_type == 'other'
+  end
+
+  def classic_call_lo_and_hi
+    lo_integer = call_lo.to_i
+    hi_integer = call_hi.to_i
+    message = 'Low and hi callnum range must be all digits, with lo lower than hi.'
+    errors.add(:base, message) unless call_lo =~ /^[0-9]+$/ && call_hi =~ /^[0-9]+$/ && lo_integer < hi_integer
   end
 end
