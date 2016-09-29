094b9250a8f59fbd9b3b9a09f35fdb87eb0006e7
Tony Zanella <tony.zanella@gmail.com>
2016-09-29 15:14:17 -0700

    Redirect to new view instead of root_url when validations fail
      Also, separate new from create specs

--- spec/controllers/expenditures_with_circ_stats_reports_controller_spec.rb
+++ spec/controllers/expenditures_with_circ_stats_reports_controller_spec.rb
@@ -7,20 +7,20 @@ RSpec.describe ExpendituresWithCircStatsReportsController, type: :controller do
       get 'new'
       expect(response).to render_template('new')
     end
-    describe 'post#create' do
-      it 'returns 302 when saving sal3_batch_requests_batch' do
-        stub_current_user(FactoryGirl.create(:authorized_user))
-        post :create, expenditures_with_circ_stats_report: { email: 'someone@some.one',
-                                                             fund: '1008930-1-HAGOY',
-                                                             lib_array: %w(GREEN),
-                                                             fmt_array: %w(MANUSCRPT MAP MARC) }
-        expect(response).to have_http_status(302)
-      end
-      it 'renders new template with an invalid object' do
-        stub_current_user(FactoryGirl.create(:authorized_user))
-        post :create, expenditures_with_circ_stats_report: { email: '' }
-        expect(response).to redirect_to root_path
-      end
+  end
+  describe 'post#create' do
+    it 'returns 302 when saving sal3_batch_requests_batch' do
+      stub_current_user(FactoryGirl.create(:authorized_user))
+      post :create, expenditures_with_circ_stats_report: { email: 'someone@some.one',
+                                                           fund: '1008930-1-HAGOY',
+                                                           lib_array: %w(GREEN),
+                                                           fmt_array: %w(MANUSCRPT MAP MARC) }
+      expect(response).to have_http_status(302)
+    end
+    it 'renders new template with an invalid object' do
+      stub_current_user(FactoryGirl.create(:authorized_user))
+      post :create, expenditures_with_circ_stats_report: { email: '' }
+      expect(response).to render_template('new')
     end
   end
 end
