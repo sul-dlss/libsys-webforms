9739e6460494302a4b8971e1ac38937bdb334f53
Tony Zanella <tony.zanella@gmail.com>
2016-09-29 16:30:45 -0700

    Invalid encumbrance report redirects to new. Fixes #211

--- spec/controllers/encumbrance_reports_controller_spec.rb
+++ spec/controllers/encumbrance_reports_controller_spec.rb
@@ -7,20 +7,20 @@ RSpec.describe EncumbranceReportsController, type: :controller do
       get 'new'
       expect(response).to render_template('new')
     end
-    describe 'post#create' do
-      it 'returns 302 when saving sal3_batch_requests_batch' do
-        stub_current_user(FactoryGirl.create(:authorized_user))
-        post :create, encumbrance_report: { email: 'someone@some.one',
-                                            fund: '1008930-1-HAGOY',
-                                            fundcyc_cycle: '2016' }
+  end
+  describe 'post#create' do
+    it 'returns 302 when saving encumbrance report' do
+      stub_current_user(FactoryGirl.create(:authorized_user))
+      post :create, encumbrance_report: { email: 'someone@some.one',
+                                          fund: '1008930-1-HAGOY',
+                                          fundcyc_cycle: '2016' }
 
-        expect(response).to have_http_status(302)
-      end
-      it 'renders new template with an invalid object' do
-        stub_current_user(FactoryGirl.create(:authorized_user))
-        post :create, encumbrance_report: { fund: '' }
-        expect(response).to redirect_to root_path
-      end
+      expect(response).to have_http_status(302)
+    end
+    it 'renders new template with an invalid object' do
+      stub_current_user(FactoryGirl.create(:authorized_user))
+      post :create, encumbrance_report: { fund: '' }
+      expect(response).to render_template('new')
     end
   end
 end
