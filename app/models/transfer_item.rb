082555a05ae0daf2bbe27546daf4a7f0fa0ae671
Darsi Rueda <dlrueda@stanford.edu>
2016-10-19 16:19:04 -0700

    make itype optional on tf, add unicorn_policies seed data

--- app/models/transfer_item.rb
+++ app/models/transfer_item.rb
@@ -7,6 +7,6 @@ class TransferItem
   attr_accessor :current_library, :new_library, :new_homeloc, :new_curloc, :new_item_type,
                 :item_ids, :email, :comments, :batch_date, :user_name, :action,
                 :priority, :export_yn, :check_bc_first
-  validates :current_library, :new_library, :new_homeloc, :new_item_type, :item_ids,
+  validates :current_library, :new_library, :new_homeloc, :item_ids,
             presence: true
 end
