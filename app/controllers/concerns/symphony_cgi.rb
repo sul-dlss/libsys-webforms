a3a82a5b0ea7ba9e9dd6d16b01e5663edb06174d
Joshua Greben <jgreben@stanford.edu>
2016-10-24 16:13:52 -0700

    Try constructing the url a different way to avoid bad URI error

--- app/controllers/concerns/symphony_cgi.rb
+++ app/controllers/concerns/symphony_cgi.rb
@@ -8,20 +8,18 @@ module SymphonyCgi
   include ShelfSelectionParams
 
   def request_conn(script, cgi_params)
-    conn = Faraday.new(url: "#{Settings.symphony_cgi_url}#{script}") do |symphony|
+    conn = Faraday.new(url: Settings.symphony_cgi_url) do |symphony|
       symphony.request  :url_encoded             # form-encode POST params
       symphony.response :logger                  # log requests to STDOUT
       symphony.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
 
-    conn.get(hash_to_query(cgi_params))
+    conn.get do |req|
+      req.url "/#{script}"
+      cgi_params.map {|k,v| req.params[k] = "#{v}"}
+    end
 
   rescue Faraday::SSLError, Faraday::Error::ConnectionFailed
     NullResponse.new
-  end
-
-  def hash_to_query(cgi_params)
-    u = URI.encode_www_form(cgi_params)
-    u.split('&').select { |param| param =~ /=./ }.join('&')
   end
 end
