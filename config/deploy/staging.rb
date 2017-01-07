958b7ed6b5e7ef7924bd8b24dfbe7faf2ae09503
Tony Zanella <tony.zanella@gmail.com>
2017-01-06 16:57:45 -0800

    stage in rails production environment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 server 'libsys-webforms-stage.stanford.edu', user: 'libsys', roles: %w{app db web}
 
 Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, 'staging'
+set :rails_env, 'production'
 
 set :delayed_job_workers, 4
