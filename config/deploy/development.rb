14b48c894906eb75d4f7440e7b953b26fcc6c75d
Tony Zanella <tony.zanella@gmail.com>
2017-01-13 12:21:16 -0800

    Use sidekiq.yml
      Related to sul-dlss/libsys-webforms#318

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -3,5 +3,3 @@ server 'libsys-webforms-dev.stanford.edu', user: 'libsys', roles: %w{app db web}
 Capistrano::OneTimeKey.generate_one_time_key!
 
 set :rails_env, 'development'
-
-set :delayed_job_workers, 4
