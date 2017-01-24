0c8769e4a49634cf2c16142bb8f174487a13c926
Joshua Greben <jgreben@stanford.edu>
2017-01-24 10:24:02 -0800

    App throws error if FY date is not in table, rescue from exception

--- app/models/expenditure_report.rb
+++ app/models/expenditure_report.rb
@@ -59,11 +59,13 @@ class ExpenditureReport < ActiveRecord::Base
 
   def write_fy_start(year)
     fy_start = ExpendituresFyDate.find(year).min_paydate.strftime('%Y-%^b-%d')
+  rescue ActiveRecord::RecordNotFound
     write_range_start(fy_start)
   end
 
   def write_fy_end(year)
     fy_end = ExpendituresFyDate.find(year).max_paydate.strftime('%Y-%^b-%d')
+  rescue ActiveRecord::RecordNotFound
     write_range_end(fy_end)
   end
 
