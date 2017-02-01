19b3b365446735bf0d8298ae6e31d3fbb868187a
Tony Zanella <tony.zanella@gmail.com>
2017-02-01 15:39:00 -0800

    Send email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  # Send email in development.
+  config.action_mailer.default_options = { from: 'no-reply@libsys-webforms-prod.stanford.edu' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
