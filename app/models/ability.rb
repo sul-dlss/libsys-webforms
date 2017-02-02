9440d0417cf5b5440404835d92f84329ae180dd6
Joshua Greben <jgreben@stanford.edu>
2017-02-02 09:59:59 -0800

    Refine permissions so that more users can view Errors for Batch

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,20 @@ class Ability
 
   def initialize(current_user)
     current_user ||= AuthorizedUser.new
+    assign_staff_permission(current_user)
+    assign_basic_permission if current_user
+    assign_batch_permission if current_user.unicorn_updates == 'Y'
+  end
+
+  def assign_basic_permission
+    can :manage, BatchRecordUpdate
+  end
+
+  def assign_staff_permission(current_user)
     can :manage, ManagementReport if current_user.mgt_rpts == 'Y'
     can :create, Sal3BatchRequestsBatch if current_user.sal3_batch_req == 'Y'
     can :create, UserloadRerun if current_user.userload_rerun == 'Y'
     can [:read, :update], Sal3BatchRequestsBatch if current_user.sal3_breq_edit == 'Y'
-    assign_batch_permission if current_user.unicorn_updates == 'Y'
   end
 
   def assign_batch_permission
