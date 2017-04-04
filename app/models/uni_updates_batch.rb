8c3e64e4fa0aa95f35c04396651b0e78b518b1f5
Darren Hardy <drh@stanford.edu>
2017-04-04 11:11:22 -0700

    bump AbcSize and remove old Rails/HttpPositionalArguments

--- app/models/uni_updates_batch.rb
+++ app/models/uni_updates_batch.rb
@@ -6,7 +6,6 @@ class UniUpdatesBatch < ActiveRecord::Base
   self.primary_key = 'batch_id'
   has_many :uni_updates, foreign_key: 'batch_id', class_name: UniUpdates, dependent: :destroy
 
-  # rubocop:disable Metrics/AbcSize
   def self.create_item_type_batch(params, total_bcs)
     create(batch_date: params[:change_item_type][:load_date],
            user_name: params[:change_item_type][:user_name],
@@ -86,5 +85,4 @@ class UniUpdatesBatch < ActiveRecord::Base
            pending: 'Y',
            total_bcs: total_bcs)
   end
-  # rubocop:enable Metrics/AbcSize
 end
