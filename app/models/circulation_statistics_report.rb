531351286b289d5eab66765beaa89d21b483bd48
Joshua Greben <jgreben@stanford.edu>
2017-10-10 14:24:24 -0700

    Make sure call letters are uppercase if they are submitted in the form

--- app/models/circulation_statistics_report.rb
+++ app/models/circulation_statistics_report.rb
@@ -25,7 +25,17 @@ class CirculationStatisticsReport
   validate :classic_call_alpha, if: :classic_range_type?
   validates :email, format: { with: Rails.configuration.email_pattern }, allow_blank: true
 
+  before_validation :upcase_call_alpha
+
   private
+
+  def upcase_call_alpha
+    call_alpha.upcase! unless call_alpha.nil?
+  end
+
+  def upcase_call_lo
+    call_lo.upcase! unless call_lo.nil?
+  end
 
   def barcode_range_type?
     range_type == 'barcodes'
