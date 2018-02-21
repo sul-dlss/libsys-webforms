c18d304778a1b0cda9924050f3ce982a15624771
Joshua Greben <jgreben@stanford.edu>
2018-02-21 15:48:18 -0800

    Fix typo, column name, and add test for other table headers

--- spec/features/sal3_batch_request_batches_spec.rb
+++ spec/features/sal3_batch_request_batches_spec.rb
@@ -9,5 +9,8 @@ describe 'sal3_batch_requests_batches' do
     expect(page).to have_css('th', text: 'Name or short description')
     expect(page).to have_css('th', text: 'Stop code')
     expect(page).to have_css('th', text: 'No. of items requested')
+    expect(page).to have_css('th', text: 'Status')
+    expect(page).to have_css('th', text: 'Multiple requests in batch?')
+    expect(page).to have_css('th', text: 'Date requested')
   end
 end
