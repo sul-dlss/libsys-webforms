b361d307080512ed5cc155d263ba099c09f96c57
Shelley Doljack <sdoljack@stanford.edu>
2018-03-01 14:48:50 -0800

    Adds custom datatable sort so blank fields sort at bottom.

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,4 +1,26 @@
+
 $(document).on('ready page:load', function () {
-  $('table').DataTable();
+  jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+    "non-empty-string-asc": function (str1, str2) {
+      if(str1 === "")
+      return 1;
+      if(str2 === "")
+      return -1;
+      return ((str1 < str2) ? -1 : ((str1 > str2) ? 1 : 0));
+    },
+
+    "non-empty-string-desc": function (str1, str2) {
+      if(str1 === "")
+      return 1;
+      if(str2 === "")
+      return -1;
+      return ((str1 < str2) ? 1 : ((str1 > str2) ? -1 : 0));
+    }
+  } );
+  $('table').DataTable({
+      columnDefs: [
+         {type: 'non-empty-string', targets: 0} // define 'name' column as non-empty-string type
+      ]
+  });
   $("[data-toggle='tooltip']").tooltip();
 });
