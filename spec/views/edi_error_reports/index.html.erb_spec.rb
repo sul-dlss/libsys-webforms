697542d9db89facbc66b12b97289b59a06838696
Joshua Greben <jgreben@stanford.edu>
2018-07-16 11:51:40 -0700

    Adds error level and type to page header when scope is applied; removes accordion from EDI menu

--- spec/views/edi_error_reports/index.html.erb_spec.rb
+++ spec/views/edi_error_reports/index.html.erb_spec.rb
@@ -17,4 +17,25 @@ RSpec.describe 'edi_error_reports/index', type: :view do
       assert_select 'td', text: 'notify'.to_s
     end
   end
+  context 'error level scope' do
+    before :each do
+      stub_current_user_for_view { FactoryBot.create(:staff_user) }
+      assign(:edi_error_report, [FactoryBot.create(:edi_error_report)])
+    end
+    it 'should display the page header with the EDI_QUIT_ITEM.do_removes type' do
+      allow(controller).to receive(:params).and_return(type: 'EDI_QUIT_ITEM.do_removes')
+      render
+      assert_select 'h1', text: 'EDIFACT invoice errors (EDI_QUIT_ITEM.do_removes)'.to_s
+    end
+    it 'should display the page header with the fatal error level' do
+      allow(controller).to receive(:params).and_return(level: 'fatal')
+      render
+      assert_select 'h1', text: 'EDIFACT invoice errors (fatal)'.to_s
+    end
+    it 'should display the page header with the notify error level' do
+      allow(controller).to receive(:params).and_return(level: 'notify')
+      render
+      assert_select 'h1', text: 'EDIFACT invoice errors (notify)'.to_s
+    end
+  end
 end
