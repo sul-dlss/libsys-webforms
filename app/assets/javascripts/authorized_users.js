d24b809a7f47c9ea908647c451090efd3fd75fe9
Joshua Greben <jgreben@stanford.edu>
2018-08-07 12:01:17 -0700

    Initialize clickable-row within datatable

--- app/assets/javascripts/authorized_users.js
+++ app/assets/javascripts/authorized_users.js
@@ -4,8 +4,12 @@ $(document).ready(function() {
     });
 
     $('table').DataTable({
+        clickablerow: clickRow
+    });
+
+    function clickRow() {
         $(".clickable-row").click(function() {
             window.location = $(this).data("href");
         });
-    });
+    }
 } );
