0a2aeb6399d57ef39c3d603cf8617a9d33c1a5f9
Joshua Greben <jgreben@stanford.edu>
2018-09-07 10:58:19 -0700

    Only instatiate the lobbytrack client and tiny_tds gem if accesed from a valid IP address

--- app/models/lobby_track.rb
+++ app/models/lobby_track.rb
@@ -2,19 +2,21 @@
 class LobbyTrack
   include ActiveModel::Model
 
-  require 'tiny_tds' if Settings.lobbytrack_ips.include?(ENV['HTTP_CLIENT_IP'].to_s)
+  if Settings.lobbytrack_ips.include?(ENV['HTTP_CLIENT_IP'].to_s)
+    require 'tiny_tds'
 
-  @client = TinyTds::Client.new username: Settings.lobbytrack_user, password: Settings.lobbytrack_password,
-                                host: Settings.lobbytrack_host, port: Settings.lobbytrack_port,
-                                database: Settings.lobbytrack_db
+    @client = TinyTds::Client.new username: Settings.lobbytrack_user, password: Settings.lobbytrack_password,
+                                  host: Settings.lobbytrack_host, port: Settings.lobbytrack_port,
+                                  database: Settings.lobbytrack_db
 
-  def self.query(id)
-    @client.active? ? @client.execute(sql(id)).each : @client.close
-  end
+    def self.query(id)
+      @client.active? ? @client.execute(sql(id)).each : @client.close
+    end
 
-  def self.sql(id)
-    'SELECT CardHolderID, DateIn, ReportField1, ReportField2, LookupField1' \
-    ' FROM Jolly.dbo.logAttendance' \
-    " WHERE CardHolderID = '#{id}'"
+    def self.sql(id)
+      'SELECT CardHolderID, DateIn, ReportField1, ReportField2, LookupField1' \
+      ' FROM Jolly.dbo.logAttendance' \
+      " WHERE CardHolderID = '#{id}'"
+    end
   end
 end
