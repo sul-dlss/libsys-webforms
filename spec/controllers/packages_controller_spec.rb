322c9f0e1d2d7978deaf3f52c052bade400b0afe
Shelley Doljack <sdoljack@stanford.edu>
2018-11-13 13:18:03 -0800

    Makes rubocop happy.

--- spec/controllers/packages_controller_spec.rb
+++ spec/controllers/packages_controller_spec.rb
@@ -1,63 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe PackagesController, type: :controller do
+  before do
+    @package = FactoryBot.create(:package)
+  end
+  let(:valid_attributes) do
+    { package_name: 'different name', package_status: 'Inactive',
+      vendor_name: 'different vend', data_pickup_type: 'some other' }
+  end
+  let(:invalid_attributes) { { package_name: '', package_status: 'Active', vendor_name: '', data_pickup_type: '' } }
   describe 'GET #index' do
     it 'assigns all packages as @packages' do
-      get :index
-      expect(response).to be_successful
+      expect(get(:index)).to be_successful
     end
   end
   describe 'GET #new' do
     it 'assigns a new package' do
-      get :new
-      expect(response).to render_template('new')
+      expect(get(:new)).to render_template('new')
     end
   end
   describe 'GET #show' do
     it 'shows the package details' do
-      @package = FactoryBot.create(:package)
-      get :show, id: @package
-      expect(response).to render_template('show')
+      expect(get(:show, id: @package)).to render_template('show')
     end
   end
   describe 'POST #create' do
-    let(:attributes) do
-      { package_name: 'name', package_status: 'Inactive', vendor_name: 'vend', data_pickup_type: 'some' }
-    end
     context 'with valid params' do
       it 'creates a new Package' do
-        expect { post(:create, package: attributes) }.to change(Package, :count).by(1)
+        expect { post(:create, package: valid_attributes) }.to change(Package, :count).by(1)
       end
       it 'redirects to the created package' do
-        post :create, package: attributes
+        post :create, package: valid_attributes
         expect(response).to redirect_to(Package.last)
       end
     end
     context 'with invalid params' do
       it "re-renders the 'new' template" do
-        post :create, package: { package_name: '', vendor_name: '', data_pickup_type: '' }
+        post :create, package: invalid_attributes
         expect(response).to render_template('new')
       end
     end
   end
   describe 'PUT #update' do
-    let(:valid_attributes) do
-      { package_name: 'different name', package_status: 'Inactive',
-        vendor_name: 'different vend', data_pickup_type: 'some other' }
-    end
-    let(:invalid_attributes) { { package_name: '', package_status: 'Active', vendor_name: '', data_pickup_type: '' } }
     context 'with valid params' do
-      it 'updates the requested package' do
-        package = FactoryBot.create(:package)
-        put :update, id: package[:id], package: valid_attributes
-        package.reload
+      it 'updates the requested package and redirects to the package' do
+        put :update, id: @package[:id], package: valid_attributes
+        expect(response).to redirect_to(@package)
       end
       it 'updates match_opts' do
-        valid_attributes.update(match_opts: ['0', '020', '0', '776_isbn', '0', '0', '0'])
-        package = FactoryBot.create(:package)
-        put :update, id: package[:id], package: valid_attributes
-        package.reload
-        expect(Package.find(package[:id]).match_opts).to eq('020,776_isbn')
+        valid_attributes.update(match_opts: %w[0 020 0 776_isbn 0 0 0])
+        put :update, id: @package[:id], package: valid_attributes
+        expect(Package.find(@package[:id]).match_opts).to eq('020,776_isbn')
       end
       it 'updates access_urls_plats as tab- and pipe-delimited string' do
         valid_attributes.update(url_substring: ['ieeexplore.ieee.org', ''],
@@ -65,58 +58,37 @@ RSpec.describe PackagesController, type: :controller do
                                 provider_name: ['IEEE', ''],
                                 collection_name: ['MIT Press eBooks', ''],
                                 access_type: ['purchased', ''])
-        package = FactoryBot.create(:package)
-        put :update, id: package[:id], package: valid_attributes
-        package.reload
-        expect(Package.find(package[:id]).access_urls_plats).to eq("ieeexplore.ieee.org\tIEEE Xplore Digital Library\tIEEE\tMIT Press eBooks\tpurchased|")
+        put :update, id: @package[:id], package: valid_attributes
+        expect(Package.find(@package[:id])
+          .access_urls_plats)
+          .to eq("ieeexplore.ieee.org\tIEEE Xplore Digital Library\tIEEE\tMIT Press eBooks\tpurchased|")
       end
       it 'updates ftp_file_prefix when eloader searches for files on ftp server checkbox is unchecked' do
         valid_attributes.update(no_ftp_search: '0')
-        package = FactoryBot.create(:package)
-        put :update, id: package[:id], package: valid_attributes
-        package.reload
-        expect(Package.find(package[:id]).ftp_file_prefix).to eq('NO FTP SEARCH ***')
-      end
-      it 'redirects to the package' do
-        package = FactoryBot.create(:package)
-        put :update, id: package[:id], package: valid_attributes
-        expect(response).to redirect_to(package)
+        put :update, id: @package[:id], package: valid_attributes
+        expect(Package.find(@package[:id]).ftp_file_prefix).to eq('NO FTP SEARCH ***')
       end
     end
     context 'with invalid params' do
       it "re-renders the 'edit' template" do
-        package = FactoryBot.create(:package)
-        put :update, id: package[:id], package: invalid_attributes
+        put :update, id: @package[:id], package: invalid_attributes
         expect(response).to render_template('edit')
       end
     end
   end
   describe 'activate a package' do
-    let(:active) do
-      { package_name: 'different name', package_status: 'Active',
-        vendor_name: 'different vend', data_pickup_type: 'some other' }
-    end
     it 'updates a package status and redirects to home' do
-      package = FactoryBot.create(:package)
-      put :activate, id: package[:id], package: active
-      expect(Package.find(package[:id]).package_status).to eq('Active')
+      put :activate, id: @package[:id]
+      expect(Package.find(@package[:id]).package_status).to eq('Active')
     end
   end
   describe 'deactivate a package' do
-    let(:inactive) do
-      { package_name: 'different name', package_status: 'Inactive',
-        vendor_name: 'different vend', data_pickup_type: 'some other' }
-    end
     it 'updates a package status and redirects to home' do
-      package = FactoryBot.create(:package)
-      put :deactivate, id: package[:id], package: inactive
-      expect(Package.find(package[:id]).package_status).to eq('Inactive')
+      put :deactivate, id: @package[:id]
+      expect(Package.find(@package[:id]).package_status).to eq('Inactive')
     end
   end
   describe 'DELETE #destroy' do
-    before :each do
-      @package = FactoryBot.create(:package)
-    end
     it 'destroys the requested package' do
       expect { delete :destroy, id: @package[:id] }.to change(Package, :count).by(-1)
     end
