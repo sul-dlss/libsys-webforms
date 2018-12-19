a2d3ff9d810206f0fe588577dcb99f746269d584
Shelley Doljack <sdoljack@stanford.edu>
2018-12-19 12:06:15 -0800

    Orders packages by status then package name.

--- app/assets/javascripts/packages.js
+++ app/assets/javascripts/packages.js
@@ -1,5 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 jQuery(document).ready(function($) {
-    $('table').DataTable();
+    $('table').DataTable( {
+      "order": [[ 1, "asc" ], [ 3, "asc" ]]
+    });
 });
