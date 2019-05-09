f408dd6e33a2eadf0c257163612d671c849ce216
Shelley Doljack <sdoljack@stanford.edu>
2019-05-09 14:03:13 -0700

    Removes lingering url_exclusions code.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -479,12 +478,6 @@ ActiveRecord::Schema.define(version: 20181121214017) do
     t.string   "destination"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "url_exclusions", force: :cascade do |t|
-    t.string   "url_substring"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
   end
 
   create_table "vnd_files_gen", force: :cascade do |t|
