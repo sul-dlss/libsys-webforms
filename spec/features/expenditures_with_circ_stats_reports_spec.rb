6f1cb91c5706c2907b2a85256cf58c729d3a2ea0
Joshua Greben <jgreben@stanford.edu>
2020-01-13 13:23:00 -0800

    Fix rubocop complaints

--- spec/features/expenditures_with_circ_stats_reports_spec.rb
+++ spec/features/expenditures_with_circ_stats_reports_spec.rb
@@ -7,8 +7,8 @@ describe 'Expenditures With Circ Stats Reports Page', type: :feature do
 
   it 'renders the hidden field for date_request' do
     attribute = page.find('#expenditures_with_circ_stats_report_date_request', visible: false).value
-    time = Time.parse(attribute)
-    now = Time.parse(I18n.l(Time.now.getlocal, format: :oracle))
+    time = Time.zone.parse(attribute)
+    now = Time.zone.parse(I18n.l(Time.now.getlocal, format: :oracle))
     expect(time.strftime('%F %R')).to eq now.strftime('%F %R')
   end
 end
