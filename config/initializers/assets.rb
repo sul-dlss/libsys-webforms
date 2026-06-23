c69a8b492a655c476e06d3a0c96d3efbc2f32ab1
Joshua Greben <jgreben@stanford.edu>
2026-06-23 13:10:18 -0700

    Fix sprockets 4.x crash caused by sul_styles adding a Regexp to assets.precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,10 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( accession_number_updates.js )
 Rails.application.config.assets.precompile += %w( accession_numbers.js )
+
+# sul_styles 0.2.0 adds a Regexp to precompile which sprockets 4.x doesn't support.
+# Must run after_initialize so it fires after the engine initializer adds the Regexp.
+Rails.application.config.after_initialize do
+  Rails.application.config.assets.precompile.reject! { |e| e.is_a?(Regexp) }
+  Rails.application.config.assets.precompile += %w( sul-icons.eot sul-icons.svg sul-icons.ttf sul-icons.woff )
+end
